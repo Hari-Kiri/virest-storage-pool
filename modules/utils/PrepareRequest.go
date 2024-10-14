208d9f8a6d800e79529bbc8505c8b6eded80a2c7
Big Stuff <pratamahari89@gmail.com>
2024-10-14 09:59:29 +0000

    Create utilities mdoule.

---
+++ modules/utils/PrepareRequest.go
@@ -0,0 +1,71 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"libvirt.org/go/libvirt"
+)
+
+// Prepare request for any structure. This module will check the expected HTTP request method and convert the JSON request body to
+// structure if any.
+func PrepareRequest(httpRequest *http.Request, expectedRequestMethod string, structure any) (libvirt.Error, bool) {
+	// Create libvirt error number
+	var libvirtErrorNumber libvirt.ErrorNumber
+	if expectedRequestMethod == "GET" {
+		libvirtErrorNumber = libvirt.ERR_GET_FAILED
+	}
+	if expectedRequestMethod == "POST" {
+		libvirtErrorNumber = libvirt.ERR_POST_FAILED
+	}
+	if expectedRequestMethod == "PUT" {
+		libvirtErrorNumber = libvirt.ERR_HTTP_ERROR
+	}
+	if expectedRequestMethod == "PATCH" {
+		libvirtErrorNumber = libvirt.ERR_HTTP_ERROR
+	}
+	if expectedRequestMethod == "DELETE" {
+		libvirtErrorNumber = libvirt.ERR_HTTP_ERROR
+	}
+
+	// Check http method
+	if httpRequest.Method != expectedRequestMethod {
+		return libvirt.Error{
+			Code:    libvirtErrorNumber,
+			Domain:  libvirt.FROM_NET,
+			Message: fmt.Sprintf("a HTTP %s command to failed", expectedRequestMethod),
+			Level:   libvirt.ERR_ERROR,
+		}, true
+	}
+
+	// Read request body
+	requestBody, errorReadRequestBody := io.ReadAll(httpRequest.Body)
+	if errorReadRequestBody != nil {
+		return libvirt.Error{
+			Code:    libvirt.ERR_HTTP_ERROR,
+			Domain:  libvirt.FROM_NET,
+			Message: fmt.Sprintf("%s", errorReadRequestBody),
+			Level:   libvirt.ERR_ERROR,
+		}, true
+	}
+
+	// Request body empty
+	if len(requestBody) == 0 {
+		return libvirt.Error{}, false
+	}
+
+	// Parse JSON to model
+	errorUnmarshal := json.Unmarshal(requestBody, structure)
+	if errorUnmarshal != nil {
+		return libvirt.Error{
+			Code:    libvirt.ERR_INTERNAL_ERROR,
+			Domain:  libvirt.FROM_NET,
+			Message: fmt.Sprintf("%s", errorUnmarshal),
+			Level:   libvirt.ERR_ERROR,
+		}, true
+	}
+
+	return libvirt.Error{}, false
+}
