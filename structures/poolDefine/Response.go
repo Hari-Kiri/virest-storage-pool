0cdef51eb40db325e2248d71030a42fd63042f51
Big Stuff <pratamahari89@gmail.com>
2024-10-14 12:05:30 +0000

    Change "message" field to "data".

--- structures/poolDefine/Response.go
+++ structures/poolDefine/Response.go
@@ -5,7 +5,7 @@ import "libvirt.org/go/libvirt"
 type Response struct {
 	Response bool          `json:"response"`
 	Code     int           `json:"code"`
-	Message  Uuid          `json:"message"`
+	Data     Uuid          `json:"data"`
 	Error    libvirt.Error `json:"error"`
 }
 
