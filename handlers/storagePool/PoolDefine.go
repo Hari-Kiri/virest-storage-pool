22be2df1162dae5a9fa7680f639b18b9b4bb1061
Big Stuff <pratamahari89@gmail.com>
2024-10-17 12:21:38 +0000

    Add check libvirt connection have been established or not on error prepare request.

--- handlers/storagePool/PoolDefine.go
+++ handlers/storagePool/PoolDefine.go
@@ -51,7 +51,7 @@ func PoolDefine(responseWriter http.ResponseWriter, request *http.Request) {
 		defer waitGroup.Done()
 	}()
 	waitGroup.Wait()
-	if libvirtError.Code == 11 {
+	if libvirtError.Code == 11 && qemuConnection != nil {
 		qemuConnection.Close()
 	}
 	if isError {
