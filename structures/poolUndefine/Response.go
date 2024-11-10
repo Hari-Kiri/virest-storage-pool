86d7cc8116aead8398ed89d2938437a7f62418ee
Big Stuff <pratamahari89@gmail.com>
2024-11-10 12:55:53 +0000

    Add data field.

--- structures/poolUndefine/Response.go
+++ structures/poolUndefine/Response.go
@@ -5,5 +5,6 @@ import "libvirt.org/go/libvirt"
 type Response struct {
 	Response bool          `json:"response"`
 	Code     int           `json:"code"`
+	Data     struct{}      `json:"data"`
 	Error    libvirt.Error `json:"error"`
 }
