6f7997149db26aa0ace77d26f8c2771509df6a86
Hari-Kiri <pratamahari89@gmail.com>
2024-12-06 12:03:13 +0000

    Add generic for response parameter.

--- modules/utils/JsonResponseBuilder.go
+++ modules/utils/JsonResponseBuilder.go
@@ -4,10 +4,18 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
+
+	"github.com/Hari-Kiri/virest-storage-pool/structures/poolDefine"
+	"github.com/Hari-Kiri/virest-storage-pool/structures/poolList"
+	"github.com/Hari-Kiri/virest-storage-pool/structures/poolUndefine"
 )
 
+type ResponseStructure interface {
+	poolList.Response | poolDefine.Response | poolUndefine.Response
+}
+
 // Create http response with Content-Type: application/json.
-func JsonResponseBuilder(response interface{}, httpResponseWriter http.ResponseWriter, httpStatusCode int) {
+func JsonResponseBuilder[Response ResponseStructure](response Response, httpResponseWriter http.ResponseWriter, httpStatusCode int) {
 	var responseBuffer bytes.Buffer
 	json.NewEncoder(&responseBuffer).Encode(&response)
 	httpResponseWriter.Header().Set("Content-Type", "application/json")
