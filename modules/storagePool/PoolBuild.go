2537b4931f277968ed8160ca3fc213571d1ec622
Hari-Kiri <pratamahari89@gmail.com>
2024-12-14 19:41:50 +0000

    feat: create module and structure

---
+++ modules/storagePool/PoolBuild.go
@@ -0,0 +1,45 @@
+package storagePool
+
+import (
+	"fmt"
+
+	"libvirt.org/go/libvirt"
+)
+
+// Build the underlying storage pool.
+//
+// connection:
+//
+//	pointer to hypervisor connection
+//
+// option:
+//
+//	bitwise-OR of libvirt.StoragePoolBuildFlags
+//
+// Returns:
+//
+//	libvirt.error nil on success, or libvirt.error not nil upon failure
+func PoolBuild(connection *libvirt.Connect, poolUuid string, option libvirt.StoragePoolBuildFlags) (libvirt.Error, bool) {
+	var (
+		libvirtError libvirt.Error
+		isError      bool
+	)
+
+	// Get libvirt storage pool object
+	storagePoolObject, errorGetStoragePoolObject := connection.LookupStoragePoolByUUIDString(poolUuid)
+	libvirtError, isError = errorGetStoragePoolObject.(libvirt.Error)
+	if isError {
+		libvirtError.Message = fmt.Sprintf("failed get storage pool object: %s", libvirtError.Message)
+		return libvirtError, isError
+	}
+	defer storagePoolObject.Free()
+
+	// Undefine pool
+	libvirtError, isError = storagePoolObject.Build(option).(libvirt.Error)
+	if isError {
+		libvirtError.Message = fmt.Sprintf("failed to undefine pool: %s", libvirtError.Message)
+		return libvirtError, isError
+	}
+
+	return libvirtError, false
+}
---
+++ structures/poolBuild/Request.go
@@ -0,0 +1,6 @@
+package poolBuild
+
+type Request struct {
+	Uuid   string `json:"uuid"`
+	Option int    `json:"option"`
+}
