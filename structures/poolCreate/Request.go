2aa8b32ffcac63fa334cd042706a18f61b0502d5
Hari-Kiri <pratamahari89@gmail.com>
2024-12-15 12:49:10 +0000

    fix(pool-build): update option to libvirt.StoragePoolCreateFlags

--- structures/poolCreate/Request.go
+++ structures/poolCreate/Request.go
@@ -3,6 +3,6 @@ package poolCreate
 import "libvirt.org/go/libvirt"
 
 type Request struct {
-	Uuid   string                        `json:"uuid"`
-	Option libvirt.StoragePoolBuildFlags `json:"option"`
+	Uuid   string                         `json:"uuid"`
+	Option libvirt.StoragePoolCreateFlags `json:"option"`
 }
