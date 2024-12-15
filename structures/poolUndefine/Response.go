f6d72da5bc1bee5bc9d0edf5d37ff07f8a0033a6
Hari-Kiri <pratamahari89@gmail.com>
2024-12-15 07:34:23 +0000

    perf: delete data field

--- structures/poolUndefine/Response.go
+++ structures/poolUndefine/Response.go
@@ -5,6 +5,5 @@ import "libvirt.org/go/libvirt"
 type Response struct {
 	Response bool          `json:"response"`
 	Code     int           `json:"code"`
-	Data     struct{}      `json:"data"`
 	Error    libvirt.Error `json:"error"`
 }
