0cab7c79c7785e418edbbd4f7b097543ff312391
Hari-Kiri <pratamahari89@gmail.com>
2024-12-15 12:47:33 +0000

    feat(pool-create): create handler function

---
+++ handlers/storagePool/PoolCreate.go
@@ -0,0 +1,56 @@
+package storagePool
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/Hari-Kiri/temboLog"
+	"github.com/Hari-Kiri/virest-storage-pool/modules/storagePool"
+	"github.com/Hari-Kiri/virest-storage-pool/structures/poolCreate"
+	"github.com/Hari-Kiri/virest-utilities/utils"
+	"libvirt.org/go/libvirt"
+)
+
+func PoolCreate(responseWriter http.ResponseWriter, request *http.Request) {
+	var (
+		connection      *libvirt.Connect
+		requestBodyData poolCreate.Request
+		httpBody        poolCreate.Response
+		libvirtError    libvirt.Error
+		isError         bool
+	)
+
+	connection, libvirtError, isError = storagePool.RequestPrecondition(request, http.MethodPatch,
+		os.Getenv("VIREST_STORAGE_POOL_CONNECTION_URI"), &requestBodyData)
+	if isError {
+		httpBody.Response = false
+		httpBody.Code = utils.HttpErrorCode(libvirtError.Code)
+		httpBody.Error = libvirtError
+		utils.JsonResponseBuilder(httpBody, responseWriter, httpBody.Code)
+		temboLog.ErrorLogging(
+			"request unexpected [ "+request.URL.Path+" ], requested from "+request.RemoteAddr+":",
+			libvirtError.Message,
+		)
+		return
+	}
+	defer connection.Close()
+
+	libvirtError, isError = storagePool.PoolCreate(connection, requestBodyData.Uuid, requestBodyData.Option)
+	if isError {
+		httpBody.Response = false
+		httpBody.Code = utils.HttpErrorCode(libvirtError.Code)
+		httpBody.Error = libvirtError
+		utils.JsonResponseBuilder(httpBody, responseWriter, httpBody.Code)
+		temboLog.ErrorLogging(
+			"failed to build pool [ "+request.URL.Path+" ], requested from "+request.RemoteAddr+":",
+			libvirtError.Message,
+		)
+		return
+	}
+
+	httpBody.Response = true
+	httpBody.Code = http.StatusOK
+	httpBody.Data.Uuid = requestBodyData.Uuid
+	utils.JsonResponseBuilder(httpBody, responseWriter, httpBody.Code)
+	temboLog.InfoLogging("pool", requestBodyData.Uuid, "built [", request.URL.Path, "]")
+}
