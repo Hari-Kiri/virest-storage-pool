c9e4cd6ccdbf30f82c8b6dbad7a3fc563fe3d70c
Hari-Kiri <pratamahari89@gmail.com>
2024-12-25 14:53:38 +0000

    feature(pool delete): change option data type to StoragePoolDeleteFlags

--- structures/poolDelete/Request.go
+++ structures/poolDelete/Request.go
@@ -3,6 +3,6 @@ package poolDelete
 import "libvirt.org/go/libvirt"
 
 type Request struct {
-	Uuid   string                        `json:"uuid"`
-	Option libvirt.StoragePoolBuildFlags `json:"option"`
+	Uuid   string                         `json:"uuid"`
+	Option libvirt.StoragePoolDeleteFlags `json:"option"`
 }
