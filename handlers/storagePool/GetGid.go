325aa48969ac1fb20105c8b83e29cd1ce864e44d
Hari-Kiri <pratamahari89@gmail.com>
2024-12-25 17:16:09 +0000

    feature(get-gid): create handler function and its structures

---
+++ handlers/storagePool/GetGid.go
@@ -0,0 +1,50 @@
+package storagePool
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/Hari-Kiri/temboLog"
+	"github.com/Hari-Kiri/virest-storage-pool/modules/storagePool"
+	"github.com/Hari-Kiri/virest-storage-pool/structures/getGid"
+	"github.com/Hari-Kiri/virest-utilities/utils"
+	"github.com/golang-jwt/jwt"
+	"libvirt.org/go/libvirt"
+)
+
+func GetGid(responseWriter http.ResponseWriter, request *http.Request) {
+	var (
+		connection      *libvirt.Connect
+		requestBodyData getGid.Request
+		httpBody        getGid.Response
+		libvirtError    libvirt.Error
+		isError         bool
+	)
+
+	connection, libvirtError, isError = storagePool.RequestPrecondition(
+		request,
+		http.MethodGet,
+		&requestBodyData,
+		os.Getenv("VIREST_STORAGE_POOL_APPLICATION_NAME"),
+		jwt.SigningMethodHS512,
+		[]byte(os.Getenv("VIREST_STORAGE_POOL_APPLICATION_JWT_SIGNATURE_KEY")),
+	)
+	if isError {
+		httpBody.Response = false
+		httpBody.Code = utils.HttpErrorCode(libvirtError.Code)
+		httpBody.Error = libvirtError
+		utils.JsonResponseBuilder(httpBody, responseWriter, httpBody.Code)
+		temboLog.ErrorLogging(
+			"request unexpected [ "+request.URL.Path+" ], requested from "+request.RemoteAddr+":",
+			libvirtError.Message,
+		)
+		return
+	}
+	defer connection.Close()
+
+	httpBody.Response = true
+	httpBody.Code = http.StatusOK
+	httpBody.Data.Gid = os.Getgid()
+	utils.JsonResponseBuilder(httpBody, responseWriter, httpBody.Code)
+	temboLog.InfoLogging("show gid for group", httpBody.Data.Gid, "[", request.URL.Path, "]")
+}
