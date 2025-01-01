956551b0b066ed022a6a3f07c3245062442b52b6
Hari-Kiri <pratamahari89@gmail.com>
2025-01-01 19:22:41 +0000

    feat(find-storage-pool-source): add marshall and unmarshall

--- structures/Sources.go
+++ structures/Sources.go
@@ -10,3 +10,15 @@ type Sources struct {
 	XMLName xml.Name                       `xml:"sources"`
 	Source  []libvirtxml.StoragePoolSource `xml:"source"`
 }
+
+func (sources *Sources) Unmarshal(doc string) error {
+	return xml.Unmarshal([]byte(doc), sources)
+}
+
+func (sources *Sources) Marshal() (string, error) {
+	doc, err := xml.MarshalIndent(sources, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return string(doc), nil
+}
