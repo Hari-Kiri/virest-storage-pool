779294f90409c5b70fc9d7f73c87cb7c51faa1b3
Hari-Kiri <pratamahari89@gmail.com>
2025-01-27 23:33:48 +0000

    feat(pool-event): update event struct

--- structures/poolEvent/Response.go
+++ structures/poolEvent/Response.go
@@ -8,17 +8,11 @@ import (
 type Response struct {
 	Response bool         `json:"response"`
 	Code     int          `json:"code"`
-	Data     Info         `json:"data"`
+	Data     Event        `json:"data"`
 	Error    virest.Error `json:"error"`
 }
 
-type Info struct {
-	Name       string                   `json:"name"`
-	Uuid       string                   `json:"uuid"`
-	State      libvirt.StoragePoolState `json:"state"`
-	Persistent bool                     `json:"persistent"`
-	Autostart  bool                     `json:"autostart"`
-	Capacity   uint64                   `json:"capacity"`
-	Allocation uint64                   `json:"allocation"`
-	Available  uint64                   `json:"available"`
+type Event struct {
+	EventRefresh   int                               `json:"eventRefresh"`
+	EventLifecycle libvirt.StoragePoolEventLifecycle `json:"eventLifecycle"`
 }
