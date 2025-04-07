4004c2f4a731d33d54e6a49f78cac0636d01537f
Hari-Kiri <pratamahari89@gmail.com>
2025-04-07 16:13:14 +0000

    feat(module-unit-test): create test to find netfs pool source and find iSCSI pool source

---
+++ modules/storagePool/FindStoragePoolSource_test.go
@@ -0,0 +1,60 @@
+package storagePool
+
+import (
+	"testing"
+
+	"github.com/Hari-Kiri/virest-utilities/utils/structures/libvirtxml"
+)
+
+const (
+	netfsHost = "192.168.122.4"
+	iscsiHost = "192.168.66.12"
+)
+
+func TestFindStoragePoolSourceNetfs(test *testing.T) {
+	poolConnection, errorGetPoolConnection, isErrorGetPoolConnection := helperTestConnection(test)
+	if isErrorGetPoolConnection {
+		test.Fatalf("find storage pool source test failed: %s", errorGetPoolConnection.Message)
+	}
+
+	test.Cleanup(func() {
+		if errorCloseConnection := poolConnection.helperTestCloseConnection(test); errorCloseConnection != nil {
+			test.Errorf("connection close() failed: %s", errorCloseConnection.Error())
+		}
+	})
+
+	srcSpec := libvirtxml.Source{
+		Host: libvirtxml.Host{
+			Name: netfsHost,
+			Port: 2049,
+		},
+	}
+	_, errorFindStoragePoolSource, isErrorFindStoragePoolSource := poolConnection.FindStoragePoolSource("netfs", srcSpec)
+	if isErrorFindStoragePoolSource {
+		test.Errorf("find storage pool source test failed: %s", errorFindStoragePoolSource.Message)
+	}
+}
+
+func TestFindStoragePoolSourceIscsi(test *testing.T) {
+	poolConnection, errorGetPoolConnection, isErrorGetPoolConnection := helperTestConnection(test)
+	if isErrorGetPoolConnection {
+		test.Fatalf("find storage pool source test failed: %s", errorGetPoolConnection.Message)
+	}
+
+	test.Cleanup(func() {
+		if errorCloseConnection := poolConnection.helperTestCloseConnection(test); errorCloseConnection != nil {
+			test.Errorf("connection close() failed: %s", errorCloseConnection.Error())
+		}
+	})
+
+	srcSpec := libvirtxml.Source{
+		Host: libvirtxml.Host{
+			Name: iscsiHost,
+			Port: 3260,
+		},
+	}
+	_, errorFindStoragePoolSource, isErrorFindStoragePoolSource := poolConnection.FindStoragePoolSource("iscsi", srcSpec)
+	if isErrorFindStoragePoolSource {
+		test.Errorf("find storage pool source test failed: %s", errorFindStoragePoolSource.Message)
+	}
+}
