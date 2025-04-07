d9f01411b89ed5fc8f126db7d07f632fe3564c46
Hari-Kiri <pratamahari89@gmail.com>
2025-04-07 16:14:25 +0000

    feat(module-unit-test): create test set pool autostart after host boot up and set pool not autostarted

---
+++ modules/storagePool/PoolAutostart_test.go
@@ -0,0 +1,41 @@
+package storagePool
+
+import "testing"
+
+const poolAutostartTestUuid = "850c4194-a85a-4a7c-9941-5b53c113ed5d"
+
+func TestPoolAutostartTrue(test *testing.T) {
+	poolConnection, errorGetPoolConnection, isErrorGetPoolConnection := helperTestConnection(test)
+	if isErrorGetPoolConnection {
+		test.Fatalf("find storage pool source test failed: %s", errorGetPoolConnection.Message)
+	}
+
+	test.Cleanup(func() {
+		if errorCloseConnection := poolConnection.helperTestCloseConnection(test); errorCloseConnection != nil {
+			test.Errorf("connection close() failed: %s", errorCloseConnection.Error())
+		}
+	})
+
+	errorPoolAutostart, isErrorPoolAutostart := poolConnection.PoolAutostart(poolAutostartTestUuid, true)
+	if isErrorPoolAutostart {
+		test.Errorf("find storage pool source test failed: %s", errorPoolAutostart.Message)
+	}
+}
+
+func TestPoolAutostartFalse(test *testing.T) {
+	poolConnection, errorGetPoolConnection, isErrorGetPoolConnection := helperTestConnection(test)
+	if isErrorGetPoolConnection {
+		test.Fatalf("find storage pool source test failed: %s", errorGetPoolConnection.Message)
+	}
+
+	test.Cleanup(func() {
+		if errorCloseConnection := poolConnection.helperTestCloseConnection(test); errorCloseConnection != nil {
+			test.Errorf("connection close() failed: %s", errorCloseConnection.Error())
+		}
+	})
+
+	errorPoolAutostart, isErrorPoolAutostart := poolConnection.PoolAutostart(poolAutostartTestUuid, false)
+	if isErrorPoolAutostart {
+		test.Errorf("find storage pool source test failed: %s", errorPoolAutostart.Message)
+	}
+}
