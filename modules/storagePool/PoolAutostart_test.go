839e15629ebfdf7c2fe8d487944332b4ab29ed14
Hari-Kiri <pratamahari89@gmail.com>
2025-08-11 13:35:00 +0000

    feat(modules-unit-test): delete hardcoded storage pool uuid then using defined storage pool uuid

--- modules/storagePool/PoolAutostart_test.go
+++ modules/storagePool/PoolAutostart_test.go
@@ -2,21 +2,28 @@ package storagePool
 
 import "testing"
 
-const poolAutostartTestUuid = "850c4194-a85a-4a7c-9941-5b53c113ed5d"
-
 func TestPoolAutostartTrue(test *testing.T) {
 	poolConnection, errorGetPoolConnection, isErrorGetPoolConnection := helperTestConnection(test)
 	if isErrorGetPoolConnection {
 		test.Fatalf("connecting to host storage pool failed: %s", errorGetPoolConnection.Message)
 	}
 
+	poolUuid, errorPoolDefine, isErrorPoolDefine := poolConnection.helperTestPoolDefine(test, storagePoolDirectory, 0)
+	if isErrorPoolDefine {
+		test.Fatalf("defining pool test failed: %s", errorPoolDefine.Message)
+	}
+
 	test.Cleanup(func() {
+		if errorPoolUndefine, isErrorPoolUndefine := poolConnection.helperTestPoolUndefine(test, poolUuid); isErrorPoolUndefine {
+			test.Errorf("pool undefine failed: %s", errorPoolUndefine.Message)
+		}
+
 		if errorCloseConnection, isErrorCloseConnection := poolConnection.helperTestCloseConnection(test); isErrorCloseConnection {
 			test.Errorf("connection close() failed: %s", errorCloseConnection.Message)
 		}
 	})
 
-	errorPoolAutostart, isErrorPoolAutostart := poolConnection.PoolAutostart(poolAutostartTestUuid, true)
+	errorPoolAutostart, isErrorPoolAutostart := poolConnection.PoolAutostart(poolUuid, true)
 	if isErrorPoolAutostart {
 		test.Errorf("turn on pool autostart test failed: %s", errorPoolAutostart.Message)
 	}
@@ -28,14 +35,28 @@ func TestPoolAutostartFalse(test *testing.T) {
 		test.Fatalf("connecting to host storage pool failed: %s", errorGetPoolConnection.Message)
 	}
 
+	poolUuid, errorPoolDefine, isErrorPoolDefine := poolConnection.helperTestPoolDefine(test, storagePoolDirectory, 0)
+	if isErrorPoolDefine {
+		test.Fatalf("defining pool test failed: %s", errorPoolDefine.Message)
+	}
+
+	errorPoolAutostart, isErrorPoolAutostart := poolConnection.PoolAutostart(poolUuid, true)
+	if isErrorPoolAutostart {
+		test.Fatalf("turn on pool autostart test failed: %s", errorPoolAutostart.Message)
+	}
+
 	test.Cleanup(func() {
+		if errorPoolUndefine, isErrorPoolUndefine := poolConnection.helperTestPoolUndefine(test, poolUuid); isErrorPoolUndefine {
+			test.Errorf("pool undefine failed: %s", errorPoolUndefine.Message)
+		}
+
 		if errorCloseConnection, isErrorCloseConnection := poolConnection.helperTestCloseConnection(test); isErrorCloseConnection {
 			test.Errorf("connection close() failed: %s", errorCloseConnection.Message)
 		}
 	})
 
-	errorPoolAutostart, isErrorPoolAutostart := poolConnection.PoolAutostart(poolAutostartTestUuid, false)
-	if isErrorPoolAutostart {
-		test.Errorf("turn off pool autostart test failed: %s", errorPoolAutostart.Message)
+	errorPoolDisableAutostart, isErrorPoolDisableAutostart := poolConnection.PoolAutostart(poolUuid, false)
+	if isErrorPoolDisableAutostart {
+		test.Errorf("turn off pool autostart test failed: %s", errorPoolDisableAutostart.Message)
 	}
 }
