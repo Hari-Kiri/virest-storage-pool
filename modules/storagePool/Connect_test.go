cb8c5c2e0c70d450c0763080dc1085fd865987d0
Hari-Kiri <pratamahari89@gmail.com>
2025-08-20 16:02:41 +0000

    feat(modules-unit-test): disable connection test

--- modules/storagePool/Connect_test.go
+++ modules/storagePool/Connect_test.go
@@ -1,81 +1,81 @@
 package storagePool
 
-import (
-	"fmt"
-	"testing"
+// import (
+// 	"fmt"
+// 	"testing"
 
-	"github.com/Hari-Kiri/virest-utilities/utils"
-	"github.com/Hari-Kiri/virest-utilities/utils/structures/virest"
-	"libvirt.org/go/libvirt"
-)
+// 	"github.com/Hari-Kiri/virest-utilities/utils"
+// 	"github.com/Hari-Kiri/virest-utilities/utils/structures/virest"
+// 	"libvirt.org/go/libvirt"
+// )
 
-const hypervisorUri = "qemu:///system"
+// const hypervisorUri = "qemu:///system"
 
-func TestConnectionReadOnly(test *testing.T) {
-	virestConnection, errorConnect, isErrorConnect := utils.NewConnectWithAuth(hypervisorUri, nil, libvirt.CONNECT_RO)
-	if isErrorConnect {
-		test.Fatalf("connection test failed: %s", errorConnect.Message)
-	}
+// func TestConnectionReadOnly(test *testing.T) {
+// 	virestConnection, errorConnect, isErrorConnect := utils.NewConnectWithAuth(hypervisorUri, nil, libvirt.CONNECT_RO)
+// 	if isErrorConnect {
+// 		test.Fatalf("connection test failed: %s", errorConnect.Message)
+// 	}
 
-	result, errorResult := virestConnection.Close()
-	if errorResult != nil {
-		test.Fatalf("close() error: %s", errorResult.Error())
-	}
-	if result != 0 {
-		test.Errorf("close() == %d, expected 0", result)
-	}
-}
+// 	result, errorResult := virestConnection.Close()
+// 	if errorResult != nil {
+// 		test.Fatalf("close() error: %s", errorResult.Error())
+// 	}
+// 	if result != 0 {
+// 		test.Errorf("close() == %d, expected 0", result)
+// 	}
+// }
 
-// Not trying to resolve URI aliases
-func TestConnectionNoAliases(test *testing.T) {
-	virestConnection, errorConnect, isErrorConnect := utils.NewConnectWithAuth(hypervisorUri, nil, libvirt.CONNECT_RO)
-	if isErrorConnect {
-		test.Fatalf("connection test failed: %s", errorConnect.Message)
-	}
+// // Not trying to resolve URI aliases
+// func TestConnectionNoAliases(test *testing.T) {
+// 	virestConnection, errorConnect, isErrorConnect := utils.NewConnectWithAuth(hypervisorUri, nil, libvirt.CONNECT_RO)
+// 	if isErrorConnect {
+// 		test.Fatalf("connection test failed: %s", errorConnect.Message)
+// 	}
 
-	result, errorResult := virestConnection.Close()
-	if errorResult != nil {
-		test.Fatalf("close() error: %s", errorResult.Error())
-	}
-	if result != 0 {
-		test.Errorf("close() == %d, expected 0", result)
-	}
-}
+// 	result, errorResult := virestConnection.Close()
+// 	if errorResult != nil {
+// 		test.Fatalf("close() error: %s", errorResult.Error())
+// 	}
+// 	if result != 0 {
+// 		test.Errorf("close() == %d, expected 0", result)
+// 	}
+// }
 
-func helperTestConnection(test *testing.T) (*poolConnection, virest.Error, bool) {
-	test.Helper()
+// func helperTestConnection(test *testing.T) (*poolConnection, virest.Error, bool) {
+// 	test.Helper()
 
-	virestConnection, errorConnect, isErrorConnect := utils.NewConnectWithAuth(hypervisorUri, nil, libvirt.CONNECT_NO_ALIASES)
-	if isErrorConnect {
-		test.Fail()
-		return &poolConnection{}, errorConnect, true
-	}
+// 	virestConnection, errorConnect, isErrorConnect := utils.NewConnectWithAuth(hypervisorUri, nil, libvirt.CONNECT_NO_ALIASES)
+// 	if isErrorConnect {
+// 		test.Fail()
+// 		return &poolConnection{}, errorConnect, true
+// 	}
 
-	return &poolConnection{virestConnection.Connect}, virest.Error{}, false
-}
+// 	return &poolConnection{virestConnection.Connect}, virest.Error{}, false
+// }
 
-func (poolConnection *poolConnection) helperTestCloseConnection(test *testing.T) (virest.Error, bool) {
-	test.Helper()
+// func (poolConnection *poolConnection) helperTestCloseConnection(test *testing.T) (virest.Error, bool) {
+// 	test.Helper()
 
-	var (
-		virestError virest.Error
-		isError     bool
-	)
-	result, errorResult := poolConnection.Close()
-	virestError.Error, isError = errorResult.(libvirt.Error)
-	if isError {
-		test.Fail()
-		return virestError, isError
-	}
-	if result != 0 {
-		test.Fail()
-		return virest.Error{Error: libvirt.Error{
-			Code:    libvirt.ERR_INTERNAL_ERROR,
-			Domain:  libvirt.FROM_ACCESS,
-			Message: fmt.Sprintf("close() == %d, expected 0", result),
-			Level:   libvirt.ERR_WARNING,
-		}}, true
-	}
+// 	var (
+// 		virestError virest.Error
+// 		isError     bool
+// 	)
+// 	result, errorResult := poolConnection.Close()
+// 	virestError.Error, isError = errorResult.(libvirt.Error)
+// 	if isError {
+// 		test.Fail()
+// 		return virestError, isError
+// 	}
+// 	if result != 0 {
+// 		test.Fail()
+// 		return virest.Error{Error: libvirt.Error{
+// 			Code:    libvirt.ERR_INTERNAL_ERROR,
+// 			Domain:  libvirt.FROM_ACCESS,
+// 			Message: fmt.Sprintf("close() == %d, expected 0", result),
+// 			Level:   libvirt.ERR_WARNING,
+// 		}}, true
+// 	}
 
-	return virest.Error{}, false
-}
+// 	return virest.Error{}, false
+// }
